49f97720697494aa01c10d5831d2d757276aa26e
Anthony Wrather <github@anthonywrather.me.uk>
2025-09-01 13:17:50 +0000

    Changed the sort order of Fulfilled Orders and added some highlights to the Items displayed.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,7 @@ class Admin::OrdersController < AdminController
   # GET /admin/orders or /admin/orders.json
   def index
     @not_fulfilled_pagy, @not_fulfilled_orders = pagy(Order.where(fulfilled: false).order(created_at: :asc))
-    @fulfilled_pagy, @fulfilled_orders = pagy(Order.where(fulfilled: true).order(created_at: :asc),
+    @fulfilled_pagy, @fulfilled_orders = pagy(Order.where(fulfilled: true).order(created_at: :desc),
                                               page_param: :page_fulfilled)
   end
 
