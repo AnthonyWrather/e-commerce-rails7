1f1f5a448590cfbe83b1cb70823d2b621d277186
Anthony Wrather <github@anthonywrather.me.uk>
2025-09-05 18:14:07 +0000

    Google Analytics ID not being properly propagated.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,20 +3,11 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 document.addEventListener("turbo:load", (event) => {
-  // let googleId = document.querySelector("meta[name='google-analytics-id']").content
-  let googleId = "G-481BNJ1GVB"
-  if(googleId) {
+  if(document.querySelector("meta[name='google-analytics-id']")) {
     console.log('Got Google ID')
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', googleId);
-  } else {
-    console.log('No Google ID')
-    let googleId = "G-481BNJ1GVB"
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', googleId);
+    gtag('config', document.querySelector("meta[name='google-analytics-id']").content);
   }
 })
