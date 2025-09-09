a9cb4bef66c7cef9e653edd312e9820159471b9b
Anthony Wrather <github@anthonywrather.me.uk>
2025-09-09 23:17:53 +0000

    Set the Dashboard to be from the last calendar month starting on the 1st.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -40,5 +40,28 @@ class AdminController < ApplicationController
     end
 
     @revenue_by_day = complete_ordered_array_with_current_last
+
+    # Monthly Revenue Chart Data
+    num_orders_monthly = Order.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).count
+    num_products_monthly = OrderProduct.joins(:order).where(orders: { created_at: Time.now.beginning_of_month..Time.now.end_of_month }).sum(:quantity)
+    avg_items_monthly = 0
+    if num_orders_monthly.positive? && num_products_monthly.positive?
+      avg_items_monthly = num_products_monthly.div(num_orders_monthly)
+    end
+
+    @monthly_stats = {
+      sales: num_orders_monthly,
+      items: num_products_monthly,
+      revenue: Order.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).sum(:total)&.round(),
+      avg_sale: Order.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).average(:total)&.round(),
+      per_sale: avg_items_monthly
+    }
+
+    @monthly_orders_by_day = Order.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).order(:created_at)
+    @monthly_orders_by_day = @monthly_orders_by_day.group_by { |order| order.created_at.to_date }
+    @monthly_revenue_by_day = @monthly_orders_by_day.map { |day, orders| [day.strftime('%e %A'), orders.sum(&:total)] }
+    @days_of_month = (1..Time.days_in_month(Date.today.month, Date.today.year)).to_a
+    @monthly_revenue_by_day = @monthly_revenue_by_day.to_h
+    @revenue_by_month = @days_of_month.map { |day| [day, @monthly_revenue_by_day.fetch(Date.new(Date.today.year, Date.today.month, day).strftime('%e %A'), 0)] }
   end
 end
--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -5,13 +5,16 @@ Chart.register(...registerables)
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  static values = { revenue: Array }
+  static values = {
+    revenue: Array,
+    elementid: String
+  }
 
   initialize() {
     const data = this.revenueValue.map((item) => item[1]/100.0)
     const labels = this.revenueValue.map((item) => item[0])
 
-    const ctx = document.getElementById('revenueChart')
+    const ctx = document.getElementById(this.elementidValue)
 
     new Chart(ctx, {
       type: 'line',
