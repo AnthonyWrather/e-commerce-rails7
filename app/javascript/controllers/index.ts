0eb69550c2dce81cba2a1c8dd9322a83462cb4b9
Anthony Wrather <github@anthonywrather.me.uk>
2025-11-21 18:12:06 +0000

    convert index.js to typescript

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -1,13 +1,13 @@
 // Load all the controllers within this directory and all subdirectories.
-// Controller files must be named *_controller.js.
+// Controller files must be named *_controller.ts or *_controller.js.
 
 import { application } from "./application"
 
 // Manually register each controller
-import CartController from "./cart_controller.ts"
+import CartController from "./cart_controller"
 import ProductsController from "./products_controller"
-import DashboardController from "./dashboard_controller.ts"
-import QuantitiesController from "./quantities_controller.ts"
+import DashboardController from "./dashboard_controller"
+import QuantitiesController from "./quantities_controller"
 
 application.register("cart", CartController)
 application.register("products", ProductsController)
