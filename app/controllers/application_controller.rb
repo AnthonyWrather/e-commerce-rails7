bfaa3384e25b647d5af93909d84fdced8cf72351
Anthony Wrather <github@anthonywrather.me.uk>
2025-11-23 23:54:30 +0000

    Add deployment logging using HoneyBadger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action do
     Honeybadger.context({
-                          user_id: current_user.id,
-                          user_email: current_user.email
+                          user_id: current_user.id.exists? ? current_user.id : 'Guest',
+                          user_email: current_user.email.exists? ? current_user.email : 'none@guest.com'
                         })
   end
 end
