9ba6abef607da7d5bcc0c22b34224c9562676859
Anthony Wrather <github@anthonywrather.me.uk>
2025-11-27 01:50:21 +0000

    Rebase with main.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_204746) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_235405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,10 +103,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_204746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock_level"
-    t.integer "shipping_weight"
-    t.integer "shipping_length"
-    t.integer "shipping_width"
-    t.integer "shipping_height"
+    t.integer "weight"
+    t.integer "length"
+    t.integer "width"
+    t.integer "height"
     t.boolean "fiberglass_reinforcement", default: false, null: false
     t.integer "min_resin_per_m2", default: 0, null: false
     t.integer "max_resin_per_m2", default: 0, null: false
