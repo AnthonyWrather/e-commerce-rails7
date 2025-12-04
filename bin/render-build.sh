114d0a93d46c604bf2fca53241dd3c5977df7ec9
Anthony Wrather <github@anthonywrather.me.uk>
2025-12-04 22:13:23 +0000

    Updated Render build script.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,10 +5,11 @@ set -o errexit
 bundle install
 yarn install --frozen-lockfile
 bundle exec rails assets:precompile
+bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
-
+bundle exec rails db:prepare
 bundle exec rails db:migrate
